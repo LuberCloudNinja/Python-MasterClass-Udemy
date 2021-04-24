a25e18f2cf48109309593bf846b03156ad573508
System Administrator <root@Lubers-iMac.attlocal.net>
2021-04-23 19:00:48 -0700

    HighLowGame

---
+++ 2-Program Flow Control in Python/13-HighLowGame.py
@@ -0,0 +1,26 @@
+low = 1
+high = 1000
+
+print(f"Please think of a number between {low} and {high}")
+input("Please ENTER to start")
+
+guesses = 1
+while True:
+    guess = low + (high - low) // 2
+    high_low = input("My guess is {}. Should I guess higher or lower? Enter h or l,"
+                     "or c if my guess was correct: "
+                     .format(guess).casefold())
+
+    if high_low == "h":
+        # Guess higher. The low end of the range becomes 1 greater than the guess.
+        low = guess + 1
+    elif high_low == "l":
+        # Guess lower. The high end of the range becomes one less than the guess.
+        low = guess - 1
+    elif high_low == "c":
+        print("I got it in {} guesses!".format(guesses))
+        break
+    else:
+        print("Please enter h, l or c")
+
+    guesses = guesses + 1
