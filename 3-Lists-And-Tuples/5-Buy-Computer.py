59d2fca4d75a56c3af00011ebebd2b1297c5868e
System Administrator <root@ip-192-168-1-66.us-gov-west-1.compute.internal>
2021-04-27 10:39:32 -0700

    Removing Items from a List

--- 3-Lists-And-Tuples/5-Buy-Computer.py
+++ 3-Lists-And-Tuples/5-Buy-Computer.py
@@ -1,30 +1,39 @@
-available_parts = ["Computer",
-                   "Monitor",
-                   "Keyboard",
-                   "Mouse",
-                   "HDMI cable",
-                   "Dvd drive"
-                   ]
-# valid_choices = [str(i) for i in range(1, len(available_parts) + 1)] Will be cover in later sessions
-valid_choices = []
-for i in range(1, len(available_parts) + 1):
-    valid_choices.append(str(i))
-print(valid_choices)
-current_choice = "-"
-computer_parts = []  # create an empty list.
+def buy_computer():
+    available_parts = ["Computer",
+                       "Monitor",
+                       "Keyboard",
+                       "Mouse",
+                       "HDMI cable",
+                       "Dvd drive"
+                       ]
+    # valid_choices = [str(i) for i in range(1, len(available_parts) + 1)] Will be cover in later sessions
+    valid_choices = []
+    for i in range(1, len(available_parts) + 1):
+        valid_choices.append(str(i))
+    print(valid_choices)
+    current_choice = "-"
+    computer_parts = []  # create an empty list.
 
-while current_choice != "0":
-    if current_choice in valid_choices:
-        print(f"Adding {current_choice}")
-        index = int(current_choice) - 1
-        chosen_part = available_parts[index]
-        computer_parts.append(chosen_part)
-    else:
-        print("Please add from the list below: ")
-        for number, part in enumerate(available_parts):
-            print(f"{number + 1}: {part}")
+    while current_choice != "0":
+        if current_choice in valid_choices:
+            index = int(current_choice) - 1
+            chosen_part = available_parts[index]
+            if chosen_part in computer_parts:
+                # If item it's already in chosen_part list, remove it.
+                print(f"Removing {current_choice}")
+                computer_parts.remove(chosen_part)
+            else:
+                print(f"Adding {current_choice}")
+                computer_parts.append(chosen_part)
+                print(f"Your list now contains {computer_parts}")
+        else:
+            print("Please add from the list below: ")
+            for number, part in enumerate(available_parts):
+                print(f"{number + 1}: {part}")
 
-    current_choice = input("Please enter a value from the above list: ")
+        current_choice = input("Please enter a value from the above list: ")
 
-print(f"You added the fallowing to the list {computer_parts}: ")
+    print(f"You added the fallowing to the list {computer_parts}: ")
 
+
+buy_computer()
