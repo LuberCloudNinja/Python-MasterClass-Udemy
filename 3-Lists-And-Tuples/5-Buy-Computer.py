b52110120849dd88caeaa5b5796cde79f8e5bc85
System Administrator <root@ip-192-168-1-66.us-gov-west-1.compute.internal>
2021-04-27 08:55:32 -0700

    Iterating over a list

--- 3-Lists-And-Tuples/5-Buy-Computer.py
+++ 3-Lists-And-Tuples/5-Buy-Computer.py
@@ -1,8 +1,16 @@
+available_parts = ["Computer",
+                   "Monitor",
+                   "Keyboard",
+                   "Mouse",
+                   "Mouse mat",
+                   "HDMI cable",
+                   "Dvd drive"
+                   ]
 current_choice = "-"
 computer_parts = []  # create an empty list.
 
 while current_choice != "0":
-    if current_choice in "123456":
+    if current_choice in "1234567":
         print(f"Adding {current_choice}")
         if current_choice == "1":
             computer_parts.append("Computer".casefold())
@@ -16,16 +24,12 @@ while current_choice != "0":
             computer_parts.append("Mouse mat".casefold())
         elif current_choice == "6":
             computer_parts.append("HDMI cable".casefold())
-
+        elif current_choice == "7":
+            computer_parts.append("Dvd drive".casefold())
     else:
         print("Please add from the list below: ")
-        print("1: Computer")
-        print("2: Monitor")
-        print("3: Keyboard")
-        print("4: Mouse")
-        print("5: Mouse mat")
-        print("6: HDMI cable")
-        print("0: To finish")
+        for part in available_parts:
+            print("{0}: {1}".format(available_parts.index(part) + 1, part))
 
     current_choice = input("Please enter a value from the above list: ")
 print(f"You added the fallowing to the list {computer_parts}: ")
