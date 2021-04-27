d9a2debd68165db9b9446af7636975cf60422c03
System Administrator <root@ip-192-168-1-66.us-gov-west-1.compute.internal>
2021-04-27 09:36:15 -0700

    Improving our Code

--- 3-Lists-And-Tuples/5-Buy-Computer.py
+++ 3-Lists-And-Tuples/5-Buy-Computer.py
@@ -2,34 +2,29 @@ available_parts = ["Computer",
                    "Monitor",
                    "Keyboard",
                    "Mouse",
-                   "Mouse mat",
                    "HDMI cable",
                    "Dvd drive"
                    ]
+# valid_choices = [str(i) for i in range(1, len(available_parts) + 1)] Will be cover in later sessions
+valid_choices = []
+for i in range(1, len(available_parts) + 1):
+    valid_choices.append(str(i))
+print(valid_choices)
 current_choice = "-"
 computer_parts = []  # create an empty list.
 
 while current_choice != "0":
-    if current_choice in "1234567":
+    if current_choice in valid_choices:
         print(f"Adding {current_choice}")
-        if current_choice == "1":
-            computer_parts.append("Computer".casefold())
-        elif current_choice == "2":
-            computer_parts.append("Monitor".casefold())
-        elif current_choice == "3":
-            computer_parts.append("Keyboard".casefold())
-        elif current_choice == "4":
-            computer_parts.append("Mouse".casefold())
-        elif current_choice == "5":
-            computer_parts.append("Mouse mat".casefold())
-        elif current_choice == "6":
-            computer_parts.append("HDMI cable".casefold())
-        elif current_choice == "7":
-            computer_parts.append("Dvd drive".casefold())
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
     else:
         print("Please add from the list below: ")
         for number, part in enumerate(available_parts):
             print(f"{number + 1}: {part}")
 
     current_choice = input("Please enter a value from the above list: ")
+
 print(f"You added the fallowing to the list {computer_parts}: ")
+
