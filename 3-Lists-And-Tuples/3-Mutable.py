b27a980e761e2d8cb0dd0b52e305803d50989fcb
System Administrator <root@Lubers-iMac.attlocal.net>
2021-04-26 20:51:12 -0700

    Biding Multiple Names to a List

--- 3-Lists-And-Tuples/3-Mutable.py
+++ 3-Lists-And-Tuples/3-Mutable.py
@@ -5,7 +5,6 @@ shopping_list = ["milk",
                  "bread",
                  "rice"
                  ]
-
 another_list = shopping_list
 print(id(shopping_list))
 print(id(another_list))
@@ -13,5 +12,12 @@ print(id(another_list))
 shopping_list += ["cookies"]
 print(shopping_list)
 print(id(shopping_list))
+print(another_list)
+print()
+a = b = c = d = e = f = another_list
+print(a)
 
-
+print("Adding cream")
+b.append("cream")
+print(c)
+print(d)
