d2cb79ba31fddc8ea65bece2f6e5556774ccc4eb
System Administrator <root@Lubers-iMac.attlocal.net>
2021-04-29 08:16:04 -0700

    Functions calling functions

--- 4-Functions-Introduction/1-Functions.py
+++ 4-Functions-Introduction/1-Functions.py
@@ -3,14 +3,24 @@ def multiply(x, y):
     return result
 
 
-answer = multiply(10.5, 4)
-print(answer)
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].casefold() == string.casefold()
 
-forty_two = multiply(6, 7)
-print(forty_two)
 
-print()
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+    print(string)
+    # return string[::-1].casefold() == string.casefold()
+    return is_palindrome(string)
 
-for val in range(1, 5):
-    two_times = multiply(2, val)
-    print(two_times)
+
+word = input("Please enter a word to check: ")
+if palindrome_sentence(word):
+    print(f"'{word}' is a palindrome")
+else:
+    print(f"'{word}' is not a palindrome")
